ce92b1594bc300ea07e9170e8ee13ec585259e87
aenosys <aenosys@gmail.com>
2024-08-08 20:58:30 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Add this line to enable static export
 };
 
 module.exports = nextConfig;
